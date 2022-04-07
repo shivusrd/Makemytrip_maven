4af5365ea6de5d03496ddda821796b86cbda9ee9
shivam dubey <dubeyshivam890@gmail.com>
2022-04-07 08:52:30 +0530

    new fix

--- src/main/test/makemytrip/Makemytrip_TrainBook_test.java
+++ src/main/test/makemytrip/Makemytrip_TrainBook_test.java
@@ -1,47 +1,47 @@
-package makemytrip;
-
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
-import baselibrary.Baselibrary;
-import pages.Makemytrip_TrainBook_page;
-import pages.Makemytrip_launch_page;
-import propertyutility.PropertyUtility;
-
-public class Makemytrip_TrainBook_test extends Baselibrary
-
-
-
-{
-	
-	Makemytrip_TrainBook_page ob;
-	
-	@BeforeTest
-
-	public void launch_makemytrip() {
-		String url = PropertyUtility.getreadproperty("makemytrip");
-		getlaunch();
-		driver.get(url);
-
-		ob = new Makemytrip_TrainBook_page();
-	}
-
-	@Test(priority = 0)
-	public void BookTrainTicket_Test01() throws InterruptedException
-
-	{
-	     ob.booktrain_ticket();
-		
-
-	}
-
-	
-	
-
-public static void main(String[] args) {
-	
-}
-
-
-
-}
+//package makemytrip;
+//
+//import org.testng.annotations.BeforeTest;
+//import org.testng.annotations.Test;
+//
+//import baselibrary.Baselibrary;
+//import pages.Makemytrip_TrainBook_page;
+//import pages.Makemytrip_launch_page;
+//import propertyutility.PropertyUtility;
+//
+//public class Makemytrip_TrainBook_test extends Baselibrary
+//
+//
+//
+//{
+//	
+//	Makemytrip_TrainBook_page ob;
+//	
+//	@BeforeTest
+//
+//	public void launch_makemytrip() {
+//		String url = PropertyUtility.getreadproperty("makemytrip");
+//		getlaunch();
+//		driver.get(url);
+//
+//		ob = new Makemytrip_TrainBook_page();
+//	}
+//
+//	@Test(priority = 0)
+//	public void BookTrainTicket_Test01() throws InterruptedException
+//
+//	{
+//	     ob.booktrain_ticket();
+//		
+//
+//	}
+//
+//	
+//	
+//
+//public static void main(String[] args) {
+//	
+//}
+//
+//
+//
+//}
